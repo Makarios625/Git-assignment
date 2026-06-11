d77dfe34c0acea5146fa5bfe7cea7c515928d0e5
Makarios625 <aliceasabeaduah625@gmail.com>
2026-06-11 14:10:53 +0000

    Add files via upload

---
+++ TableAndChair.java
@@ -0,0 +1,13 @@
+public class TableAndChair
+{
+    public static void main(String[] args)
+    {
+        System.out.println("X           X");
+        System.out.println("X           X");
+        System.out.println("X           X");
+        System.out.println("XXXXX   XXXXX");
+        System.out.println("  X       X");
+        System.out.println("  X       X");
+        System.out.println("  X       X");
+    }
+};
